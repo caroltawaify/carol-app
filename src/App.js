a120e55fac61d6fd30b5ea90768a6767a8aa467a
carol <carol.tawaify96@gmail.com>
2021-11-11 11:31:55 +0200

    Fix HashRouting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Home } from './components/Home';
 import { About } from './components/About';
 import { Projects } from './components/Projects';
 import { NoMatch } from './components/NoMatch';
-// import { Layout } from './components/Layout';
 import NavigationBar from './components/NavigationBar';
 import { Footer } from './components/Footer'
 
@@ -14,16 +13,14 @@ class App extends Component {
     return (
       <React.Fragment>
         <NavigationBar />
-        {/* <Layout> */}
-         <Router>
+         <HashRouter basename="/course-directory" >
             <Switch>
               <Route exact path='/' component={Home} />
-              <Route  path='/about' component={About} />
-              <Route  path='/projects' component={Projects} />
-              <Route  component={NoMatch} />
+              <Route path='/about' component={About} />
+              <Route path='/projects' component={Projects} />
+              <Route component={NoMatch} />
             </Switch>
-        </Router>
-        {/* </Layout> */}
+        </HashRouter>
         <Footer/>
       </React.Fragment>
     );
