1287fd310711ea776b4d38c43856fe84d27b23bb
carol <carol.tawaify96@gmail.com>
2021-11-11 11:38:30 +0200

    Change href to to

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -10,9 +10,9 @@ export default class NavigationBar extends Component {
                   <Container >
                       <Navbar.Brand  className="navbar-brand order-1 mr-0 " href="https://www.horizonsinternational.org/mec" target="_blank">Sponsored by MEC Horizons</Navbar.Brand>
                             <Nav className="me-auto">
-                             <Nav.Link href="/">Home</Nav.Link>
-                             <Nav.Link href="/about">About</Nav.Link>
-                             <Nav.Link href="/projects">Projects</Nav.Link>
+                             <Nav.Link to="/">Home</Nav.Link>
+                             <Nav.Link to="/about">About</Nav.Link>
+                             <Nav.Link to="/projects">Projects</Nav.Link>
                             </Nav>
                    </Container>
                 </Navbar>
